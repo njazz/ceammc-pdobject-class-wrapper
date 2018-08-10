d51462426902cd4b6c670e26ee8bfbad9b8d67f3
Alex Nadzharov <anadjarov@gmail.com>
2018-08-10 11:15:26 +0300

    patch generation fixes

--- gen/convert.py
+++ gen/convert.py
@@ -15,7 +15,7 @@ outputFile.write("extern \"C\"{\n");
 outputFile.write("void setup(){\n\n")
 
 patchFile = open("../build/_generated.pd","w+")
-patchFile.write("#N canvas 320 320 640 480 12;\n")
+patchFile.write("#N canvas 100 100 800 600 12;\n")
 patchFile.write("#X declare -lib wrapper_library;\n")
 patchFile.write("#X obj 15 15 declare -lib wrapper_library;\n")
 objectIndex = 1
@@ -39,6 +39,9 @@ for filename in os.listdir("../to_wrap/"):
         outputFile.write("// "+filename+"\n")
         patchFile.write("#X text 15 "+str(patchYPos)+ " "+filename+";\n")
         patchYPos += 30
+        patchXPos = 90
+
+        # outputFile.write("#include \""+filename+"\"")
 
         try:
             cppHeader = CppHeaderParser.CppHeader("../to_wrap/"+filename)
@@ -50,17 +53,20 @@ for filename in os.listdir("../to_wrap/"):
 
         for c in cppHeader.classes:
             className = c
+            if cppHeader.classes[c]["namespace"]:
+                className = cppHeader.classes[c]["namespace"]+"::"+c
+
             outputFile.write("\n// Class: "+className+"\n")
-            patchFile.write("#X text "+str(patchXPos+100)+" "+str(patchYPos)+ " \ "+className+";\n")
+            patchFile.write("#X text "+str(patchXPos)+" "+str(patchYPos)+ " \ "+className+";\n")
             objectIndex += 1
 
             patchYPos += 30
 
             outputFile.write("//\n")
-            outputFile.write("WRAP_CLASS("+c+" , \""+convert_name_h(c)+"\");\n")
-            patchFile.write("#X obj "+str(patchXPos+75)+" "+str(patchYPos-30)+ " ui.bang;\n")
-            patchFile.write("#X obj "+str(patchXPos+75)+" "+str(patchYPos)+ " "+convert_name_h(c)+";\n")
-            patchFile.write("#X obj "+str(patchXPos+75)+" "+str(patchYPos+25)+ " ui.display;\n")
+            outputFile.write("WRAP_CLASS("+className+" , \""+convert_name_h(c)+"\");\n")
+            patchFile.write("#X obj "+str(patchXPos-75)+" "+str(patchYPos-30)+ " ui.bang;\n")
+            patchFile.write("#X obj "+str(patchXPos-75)+" "+str(patchYPos)+ " "+convert_name_h(c)+";\n")
+            patchFile.write("#X obj "+str(patchXPos-75)+" "+str(patchYPos+25)+ " ui.display;\n")
 
             classObjectIndex = objectIndex+2
             patchFile.write("#X connect "+str(objectIndex+1)+" 0 "+str(objectIndex+2)+" 0;")
@@ -68,7 +74,7 @@ for filename in os.listdir("../to_wrap/"):
             objectIndex += 3
 
             patchYPos += 30
-            patchYPos += 30
+            patchYPos += 60
 
             methodIndex = 0
 
@@ -94,6 +100,9 @@ for filename in os.listdir("../to_wrap/"):
 
                 if m["static"] == True:
                     methodReturn = methodReturn.replace('static ','')
+
+                if m["inline"] == True:
+                    methodReturn = methodReturn.replace('inline ','')
                     #continue
 
                 methodPointerName = "_M"+className+"__"+methodName+"__"+str(methodIndex)  #"_".join(methodTypeRaw)+"__"+methodReturnRaw
@@ -123,7 +132,6 @@ for filename in os.listdir("../to_wrap/"):
                     typeDeclare = "using "+methodPointerName+"_type = "+ methodReturn + "(*)(" + ",".join(methodType) + ");\n"
 
                 outputFile.write(typeDeclare)
-
                 outputFile.write(""+methodDeclare +"\n")
 
                 pdObjectName = convert_name_h(c)+"."+convert_name_n(m["name"])
@@ -131,19 +139,39 @@ for filename in os.listdir("../to_wrap/"):
                 wrapName = "WRAP_METHOD"
                 if m["static"] == True:
                     wrapName = "WRAP_STATIC_METHOD"
+
                 outputFile.write(wrapName+"(" + className + " , " + m["name"]+" , \"" + pdObjectName+"\","+methodPointerName+","+methodPointerName+"_type);\n")
-                patchFile.write("#X obj "+str(patchXPos)+" "+str(patchYPos)+ " "+convert_name_n(pdObjectName)+";\n")
+
+                staticObjOffset = int(m["static"])*60
+                patchFile.write("#X obj "+str(patchXPos + staticObjOffset)+" "+str(patchYPos)+ " "+convert_name_n(pdObjectName)+";\n")
                 objectIndex += 1
 
-                patchFile.write("#X connect "+str(classObjectIndex)+" 0 "+str(objectIndex)+" 0;")
+                methodInfoString = "("+" ".join(methodType)+")->"+methodReturn
+                patchFile.write("#X text "+str(patchXPos + 30 + staticObjOffset)+" "+str(patchYPos-30)+ " "+methodInfoString+";\n")
+                objectIndex += 1
 
-                patchYPos += 60
+                if m["static"] == False:
+                    patchFile.write("#X connect "+str(classObjectIndex)+" 0 "+str(objectIndex-1)+" 0;")
+
+
+                patchFile.write("#X msg "+str(patchXPos + staticObjOffset)+" "+str(patchYPos-30)+ " ;\n")
+                patchFile.write("#X obj "+str(patchXPos + staticObjOffset)+" "+str(patchYPos+30)+ " ui.display;\n")
+                patchFile.write("#X connect "+str(objectIndex+1)+" 0 "+str(objectIndex-1)+" 0;")
+                patchFile.write("#X connect "+str(objectIndex-1)+" 0 "+str(objectIndex+2)+" 0;")
+                objectIndex += 2
+
+                if m["static"]:
+                    patchFile.write("#X text "+str(patchXPos)+" "+str(patchYPos)+" static:;")
+                    objectIndex += 1
+
+                patchYPos += 105
+
 
                 outputFile.write("\n")
 
             outputFile.write("\n")
 
-            patchXPos += 210
+            patchXPos += 300
             patchYPos =  75
 
         continue
