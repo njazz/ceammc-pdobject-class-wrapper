09af806a2dff3879f587579736bd4140e3b7ecad
Alex Nadzharov <anadjarov@gmail.com>
2018-09-05 01:48:40 +0300

    generates library patch

--- gen/create_pddoc.py
+++ gen/create_pddoc.py
@@ -7,7 +7,7 @@ sys.path = ["../"] + sys.path
 
 import CppHeaderParser3 as CppHeaderParser
 
-from subprocess import call
+# from subprocess import call
 
 # camelCase -> camel-case
 def convert_name_h(name):
@@ -20,25 +20,27 @@ def convert_name_n(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1\2', s1).lower()
 
 # ---------------
+
 def write_pddoc_class_object(name,description,objectType):
-    outputFile = open("../build/doc/"+name+".pddoc","w+")
+    fileName = "../build/doc/"+objectType+".pddoc"
+    outputFile = open(fileName,"w+")
     outputFile.write("""<?xml version="1.0" encoding="utf-8"?>
 <pddoc version="1.0">
     <object name="{2}">
         <title>{2}</title>
         <info>
-            <par> {1} </par>
+            <par>{1}</par>
         </info>
         <meta>
             <authors>
-                <author> Script </author>
+                <author>Script</author>
             </authors>
-            <description> {1} </description>
-            <license> N/A </license>
-            <library> wrapper_library </library>
-            <category> all </category>
-            <keywords> none </keywords>
-            <since> 1.0 </since>
+            <description>{1}</description>
+            <license>N/A</license>
+            <library>wrapper_library </library>
+            <category>{2}</category>
+            <keywords>none</keywords>
+            <since>1.0</since>
         </meta>
         <inlets>
             <inlet>
@@ -66,6 +68,92 @@ def write_pddoc_class_object(name,description,objectType):
     </object>
 </pddoc>
     """.format(name,description,objectType))
+    # dbFile.write("{0} . .\n", objectType)
+    # if len(fileName)>0:
+    #     call(["pd_doc2pd",fileName, "--force"])
+
+#----------
+
+def write_pddoc_class_method_object(className,description,objectType, infoString):
+    fileName = "../build/doc/"+objectType+".pddoc"
+    outputFile = open(fileName,"w+")
+    outputFile.write("""<?xml version="1.0" encoding="utf-8"?>
+<pddoc version="1.0">
+    <object name="{2}">
+        <title>{2}</title>
+        <info>
+            <par> {1} </par>
+        </info>
+        <meta>
+            <authors>
+                <author> Script </author>
+            </authors>
+            <description> {2} is a method of {0}\n
+            {1} </description>
+            <license> N/A </license>
+            <library> wrapper_library </library>
+            <category>{0}</category>
+            <keywords> none </keywords>
+            <since> 1.0 </since>
+        </meta>
+        <inlets>
+            <inlet>
+                <xinfo on="any"> {3} </xinfo>
+            </inlet>
+        </inlets>
+        <outlets>
+            <outlet> {3} </outlet>
+        </outlets>
+        <example>
+            <pdascii>
+<![CDATA[
+
+[B]
+|
+[{0}]
+|  |
+|  [ui.display]
+|
+|  [B] [1.0( [symbol test( [1 2 3(
+|  |   |     |             |
+[{2}                        ]
+|
+[ui.display]
+
+[declare -lib ../wrapper_library]
+
+]]>
+            </pdascii>
+        </example>
+    </object>
+</pddoc>
+    """.format(className,description,objectType, infoString))
+    # dbFile.write("{0} . .\n", objectType)
+    # if len(fileName)>0:
+    #     call(["pd_doc2pd",fileName, "--force"])
+
+# ---------------
+
+def hasDefaultConstructor(cls):
+    ret = False
+    hasConstructor = False
+    for m in cppHeader.classes[c]["methods"]["public"]:
+
+        if m["constructor"]:
+            hasConstructor = True
+            methodType = [t["type"] for t in m["parameters"]]
+            if methodType == "":
+                ret = True
+                continue
+    #implicit
+    if (not hasConstructor) and (not ret):
+        ret = True
+    return ret
+
+# ---------------
+
+dbFileName = "../build/doc/wrapper_library.db"
+dbFile = open(dbFileName,"w+")
 
 # ---------------
 
@@ -81,4 +169,20 @@ for filename in os.listdir("../to_wrap/"):
             className = c
 
             #stub:
-            write_pddoc_class_object(c, "no description", convert_name_n(c))
+            if hasDefaultConstructor(cppHeader.classes[c]):
+                write_pddoc_class_object(c, "no description", convert_name_n(c))
+                dbFile.write("{0} . .\n".format(convert_name_n(c)))
+
+            for m in cppHeader.classes[c]["methods"]["public"]:
+                if m["template"] != False:
+                    continue
+
+                pdObjectName = convert_name_n(c)+"."+convert_name_n(m["name"])
+
+                methodType = [t["type"] for t in m["parameters"]]
+                methodReturn = m["rtnType"]
+
+                methodInfoString = "("+" ".join(methodType)+")->"+methodReturn
+
+                write_pddoc_class_method_object(convert_name_n(c), "no description", pdObjectName, methodInfoString)
+                dbFile.write("{0} . .\n".format(pdObjectName))
--- gen/run_scripts.sh
+++ gen/run_scripts.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+rm -R ../build/doc
 mkdir ../build/doc
 /usr/local/bin/python3 convert.py
 /usr/local/bin/python3 create_pddoc.py
+
+cd ../build/
+rm -R help
+mkdir help
+cd help
+
+for file in ../doc/*
+do
+/usr/local/bin/pd_doc2pd "$file" --force --xlet-db ../doc/_xlets.db
+done
+
+/usr/local/bin/pd_makelibrary --library wrapper_library ../doc/* > ../doc/_wrapper_library.xml
+cd ../doc/
+/usr/local/bin/pd_lib2pd _wrapper_library.xml
+cp wrapper_library-help.pd ../help/wrapper_library-help.pd
