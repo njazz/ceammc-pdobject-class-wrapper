96a0408c91fddf2d84d57c93bad4793faed5f78c
Alex Nadzharov <anadjarov@gmail.com>
2018-09-08 00:05:09 +0300

    AtomList -> std::set<>

--- src/_ConvertFromCEAMMCType.cpp
+++ src/_ConvertFromCEAMMCType.cpp
@@ -1,1 +1,55 @@
 #include "_Converters.hpp"
+
+#include <set>
+
+// 1. Sets
+
+template <>
+int _fromAtomList(std::set<float>& out, AtomList l)
+{
+    for (auto& e : l) {
+        out.insert(e.asFloat());
+    }
+
+    return l.size();
+}
+
+template <>
+int _fromAtomList(std::set<double>& out, AtomList l)
+{
+    for (auto& e : l) {
+        out.insert(e.asFloat());
+    }
+
+    return l.size();
+}
+
+template <>
+int _fromAtomList(std::set<int>& out, AtomList l)
+{
+    for (auto& e : l) {
+        out.insert(e.asFloat());
+    }
+
+    return l.size();
+}
+
+template <>
+int _fromAtomList(std::set<long>& out, AtomList l)
+{
+    for (auto& e : l) {
+        out.insert(e.asFloat());
+    }
+
+    return l.size();
+}
+
+template <>
+int _fromAtomList(std::set<std::string>& out, AtomList l)
+{
+    for (auto& e : l) {
+        out.insert(e.asString());
+    }
+
+    return l.size();
+}
